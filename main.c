74c031a792fd1a1b1d2222b22fe037499a2bf30f
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-07-24 09:49:54 -0700

    reduced the unnecessary outputs in the main.c

--- main.c
+++ main.c
@@ -9,18 +9,17 @@
  */
 int main(void)
 {
-	int len;
 	int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	len2 = _printf("Let's try to printf a simple sentence.\n");
+	/*len2 = printf("Let's try to printf a simple sentence.\n");*/
 	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
+	/*printf("Character:[%c]\n",'H');*/
 	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
+	/*printf("String:[%s]\n", "I am a string !");*/
+	len2 = _printf("Percent:[%%]\n");
+	/*len = printf("Percent:[%%]\n");*/
+	/*_printf("Len:[%d]\n", len);*/
 	printf("Len:[%d]\n", len2);
 	return (0);
 }
