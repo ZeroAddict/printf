4f55d399130f9bfba06c4314d394a33017acf7fc
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-24 10:01:49 -0700

    Added a new test case to main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(void)
 	int len2;
 
 	len2 = _printf("Let's try to printf a simple sentence.\n");
+	printf("Length:[%d, %i]\n", len2, len2);
 	/*len2 = printf("Let's try to printf a simple sentence.\n");*/
 	_printf("Character:[%c]\n", 'H');
 	/*printf("Character:[%c]\n",'H');*/
